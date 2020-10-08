01bed3203500bb26e98f57ec242dd10a00e6b5ee
OlegMar1 <54880893+OlegMar1@users.noreply.github.com>
2020-10-08 16:19:16 +0300

    1. improve task 7.1

--- m7/task7.1/files/FizzBuzz_unittest.py
+++ m7/task7.1/files/FizzBuzz_unittest.py
@@ -3,25 +3,23 @@ from FizzBuzz import FizzBuzz_test
 
 class TestFizzBuzz(unittest.TestCase):
 
-	def test_simple_should_return_the_number(self):
+	def test_number(self):
 		self.assertEqual(FizzBuzz_test(1), 1)
 		self.assertEqual(FizzBuzz_test(2), 2)
 		self.assertEqual(FizzBuzz_test(4), 4)
 
 	def test_fizz(self):
 		self.assertEqual(FizzBuzz_test(3), str(3) + "\tFizz")
-		self.assertEqual(FizzBuzz_test(9), str(9) + "\tFizz")
+		self.assertEqual(FizzBuzz_test(21), str(21) + "\tFizz")
 
 	def test_buzz(self):
 		self.assertEqual(FizzBuzz_test(5), str(5) + "\tBuzz")
-		self.assertEqual(FizzBuzz_test(10), str(10) + "\tBuzz")
-	
+		self.assertEqual(FizzBuzz_test(85), str(85) + "\tBuzz")
+
 	def test_fizzbuzz(self):
 		self.assertEqual(FizzBuzz_test(15), str(15) + "\tFizzBuzz")
-		self.assertEqual(FizzBuzz_test(30), str(30) + "\tFizzBuzz")
-		
+		self.assertEqual(FizzBuzz_test(75), str(75) + "\tFizzBuzz")
+
 
 if __name__ == '__main__':
 	unittest.main()
-
-
